8d241b07c1ab99d62e038deb91fbf6cd0a7235cc
Satyakrishna Jasti <satyajasti9@gmail.com>
2019-10-21 11:35:14 +0530

    Add files via upload

--- Book.java
+++ Book.java
@@ -4,35 +4,44 @@ import java.util.Scanner;
 
 public class Book {
 
-private static int d=0;
+
 private static boolean Mainmenu= true;
 private static boolean Submenu= true;
 public static int tickets=100;
 public static Scanner s=new Scanner(System.in);
-private static int n=0;
-private static int f=0;
+static int n=s.nextInt();
+//public static int n;
+public static int c;
+//public static int d1;
+public static int tmep;
+public static int d1=tickets-n;
 
-public static void book(int n,int d)
+public static void book(int n)
 {
 	System.out.println(" ");
 	System.out.println(" ");
 	System.out.print("Enter the tickets: ");
-	n=s.nextInt();
-	System.out.println(n  +" tickets are booked");
+	//n=s.nextInt();
 	System.out.println(" ");
 	System.out.println(" ");
-	
-	d=tickets-n;
-	System.out.println("Remaing Tickets: "+d);
+	System.out.println(n +" tickets are booked");
+	System.out.println(" ");
+	System.out.println(" ");
+	int d1=tickets-n;
+	System.out.println("Remaing Tickets: "+d1);
 }
 	
-public static void cancel(int c,int d,int f)
+public static void cancel(int c,int d1)
 {
 	System.out.print("Enter cancel tickets: ");
 	c=s.nextInt();
-	f=d+c;
+	System.out.println(" ");
+	System.out.println(" ");
+	int temp=d1+c;
 	System.out.println(c +" Tickets are cancelled");
-	System.out.println("Total tickets "+f);
+	System.out.println(" ");
+	System.out.println(" ");
+	System.out.println("Total tickets "+temp);
 }
 	
 	
@@ -44,7 +53,8 @@ public static void cancel(int c,int d,int f)
 		{
 			while(Submenu)
 			{
-				System.out.println(" Welcome to Bookmymovie ");
+				System.out.println("Welcome to Bookmymovie ");
+                System.out.println(" ");
                 System.out.println(" ");
                 System.out.println("B: Book a ticket");
                 System.out.println(" ");
@@ -58,18 +68,18 @@ public static void cancel(int c,int d,int f)
                 switch(select)
                 {
                 case "B":
-                        book(n,d);            	
+                        book(n);            	
                 	break;
                 	
                 	
                 case "C":
-                	cancel(d,n,f);
+                	cancel(n,d1);
                 
                 break;
                 }
-                
                 System.out.println(" ");
-                System.out.println("Would like to start another option \n1 Yes  \n2 No");
+                System.out.println(" ");
+                System.out.println("Would like to start another option \n\n1 Yes  \n\n2 No");
                 System.out.println(" ");
                 System.out.println(" ");
                 
@@ -84,7 +94,9 @@ public static void cancel(int c,int d,int f)
                 Submenu=true;
                 
                 System.out.println(" ");
-                System.out.println("Would like continue another program\n1 Yes  \n2 No");
+                System.out.println("Would like continue another program \n\n1 Yes  \n\n2 No");
+                System.out.println(" ");
+                System.out.println(" ");
                 
                 if(sc.nextInt()==1)
                 {
